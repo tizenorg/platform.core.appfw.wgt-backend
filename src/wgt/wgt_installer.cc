9c2a68ced9ff3a4ed7ad5923639a9e21654c15f4
Tomasz Iwanek <t.iwanek@samsung.com>
2016-01-28 14:09:20 +0100

    Implement PKGMGR_REQUEST_TYPE_CLEARDATA for wgt-backend
    Verify by running: pkgcmd -t wgt -c -n $pkgid_of_installed_package
    and check if all content of apps_rw/$pkgid/data/ was removed.
    
    Requires:
     - https://review.tizen.org/gerrit/58245
    
    Change-Id: I56a3435bbda9f2034784a52e0f0ab81a1e1f9ca8

--- src/wgt/wgt_installer.cc
+++ src/wgt/wgt_installer.cc
@@ -11,6 +11,7 @@
 #include <common/step/step_check_blacklist.h>
 #include <common/step/step_create_icons.h>
 #include <common/step/step_create_storage_directories.h>
+#include <common/step/step_clear_data.h>
 #include <common/step/step_copy.h>
 #include <common/step/step_copy_backup.h>
 #include <common/step/step_copy_storage_directories.h>
@@ -189,6 +190,11 @@ WgtInstaller::WgtInstaller(ci::PkgMgrPtr pkgrmgr)
       AddStep<ci::filesystem::StepRecoverStorageDirectories>();
       AddStep<ci::filesystem::StepRecoverFiles>();
       AddStep<ci::security::StepRecoverSecurity>();
+      break;
+    }
+    case ci::RequestType::Clear: {
+      AddStep<ci::configuration::StepConfigure>(pkgmgr_);
+      AddStep<ci::filesystem::StepClearData>();
       break;
     }
     default: {
