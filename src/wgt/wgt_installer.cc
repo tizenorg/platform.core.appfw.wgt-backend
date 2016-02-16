bf707a316a3cbe8539ac8d8d43bced5fa9c16f11
Arkadiusz Szulakiewicz <a.szulakiewi@partner.samsung.com>
2016-02-16 10:07:24 +0100

    Add StepCreatePerUserSharedDirectories to installation step
    This change depends on the following patch:
    https://review.tizen.org/gerrit/#/c/59452/
    
    Change-Id: If0a11756e3cafdc7aa8805c675b0e07f571625ff

--- src/wgt/wgt_installer.cc
+++ src/wgt/wgt_installer.cc
@@ -11,6 +11,7 @@
 #include <common/step/step_check_blacklist.h>
 #include <common/step/step_create_icons.h>
 #include <common/step/step_create_storage_directories.h>
+#include <common/step/step_create_per_user_shared_directories.h>
 #include <common/step/step_clear_data.h>
 #include <common/step/step_copy.h>
 #include <common/step/step_copy_backup.h>
@@ -90,6 +91,7 @@ WgtInstaller::WgtInstaller(ci::PkgMgrPtr pkgrmgr)
       AddStep<ci::filesystem::StepCreateIcons>();
       AddStep<wgt::pkgmgr::StepGenerateXml>();
       AddStep<ci::pkgmgr::StepRegisterApplication>();
+      AddStep<ci::filesystem::StepCreatePerUserSharedDirectories>();
       AddStep<ci::pkgmgr::StepRunParserPlugin>(
           ci::PluginsLauncher::ActionType::Install);
       AddStep<ci::security::StepRegisterSecurity>();
