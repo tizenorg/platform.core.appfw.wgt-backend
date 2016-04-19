99ebb49b90ec9c8da0bb8c92a230b600bf44c8ee
Kamil Rojewski <k.rojewski@samsung.com>
2016-04-19 10:43:07 +0200

    saving ime info in the manifest
    Requires:
      - https://review.tizen.org/gerrit/#/c/67903/
    
    Change-Id: Icf7c4fd9403f50ecd3db90f9f4a86ab336e9c6e1

---
+++ src/wgt/step/common/privileges.cc
@@ -0,0 +1,12 @@
+// Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by a apache 2.0 license that can be
+// found in the LICENSE file.
+#include "privileges.h"
+
+namespace wgt {
+namespace common {
+namespace privileges {
+const char kImePrivilegeName[] = "http://tizen.org/privilege/ime";
+}  // namespace privileges
+}  // namespace common
+}  // namespace wgt
