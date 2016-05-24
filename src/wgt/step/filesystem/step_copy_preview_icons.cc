5392d4795c67d433792f6cd5cfcb000785a33e80
Tomasz Iwanek <t.iwanek@samsung.com>
2016-05-24 11:18:41 +0200

    Generate widget element in platform manifest
    To verify, install widget with appwidget and:
     - check information generated in manifest file,
     - check creation of symlink for widgets in bin/ directory,
     - check preview icons in shared/res/ directory.
    
    Requires:
     - https://review.tizen.org/gerrit/71235
    
    Change-Id: I2a38aaee729c4c1f18200d2f63764c4c87e7d730

---
+++ src/wgt/step/filesystem/step_copy_preview_icons.cc
@@ -0,0 +1,52 @@
+// Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by an apache-2.0 license that can be
+// found in the LICENSE file.
+
+#include "wgt/step/filesystem/step_copy_preview_icons.h"
+
+#include <boost/filesystem/path.hpp>
+#include <common/utils/file_util.h>
+
+#include <string>
+
+#include "wgt/wgt_backend_data.h"
+
+namespace bf = boost::filesystem;
+namespace ci = common_installer;
+
+namespace {
+
+const char kResWgt[] = "res/wgt";
+const char kSharedRes[] = "shared/res";
+
+}  // namespace
+
+namespace wgt {
+namespace filesystem {
+
+ci::Step::Status StepCopyPreviewIcons::process() {
+  WgtBackendData* backend_data =
+      static_cast<WgtBackendData*>(context_->backend_data.get());
+  for (auto& appwidget : backend_data->appwidgets.get().app_widgets()) {
+    for (auto& size : appwidget.content_size) {
+      if (!size.preview.empty()) {
+        bf::path icon_path =
+            context_->pkg_path.get() / kResWgt / size.preview;
+        std::string type = wgt::parse::AppWidgetSizeTypeToString(size.type);
+        std::string icon_name = appwidget.id + "." + type + "." + "preview" +
+            bf::path(size.preview).extension().string();
+        bf::path preview_icon =
+            context_->pkg_path.get() / kSharedRes / icon_name;
+        if (!ci::CopyFile(icon_path, preview_icon)) {
+          LOG(ERROR) << "Cannot create preview icon: " << preview_icon;
+          return Status::ICON_ERROR;
+        }
+      }
+    }
+  }
+  LOG(DEBUG) << "Preview icons created";
+  return Status::OK;
+}
+
+}  // namespace filesystem
+}  // namespace wgt
